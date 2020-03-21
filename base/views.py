7b1721ea3e2c601858ffa2721605ebfc3c832dc2
mahima-c <13as1827000713@gmail.com>
2020-03-21 15:41:09 +0530

    charge create ,and customer create in payment

--- base/views.py
+++ base/views.py
@@ -3,15 +3,34 @@ from django.urls import reverse
 from django.http import JsonResponse
 
 # Create your views here.
+import stripe
+stripe.api_key = "sk_test_PCgOc9qmyp5CBluQomn2Oqn100MzcTYVHQ"
 
 def index(request):
 	return render(request, 'base/index.html')
 
 
 def charge(request):
-	amount = 5
+	
 	if request.method == 'POST':
 		print('Data:', request.POST)
+
+
+		amount = int(request.POST['amount'])
+		customer = stripe.Customer.create(
+			email=request.POST['email'],
+			name=request.POST['nickname'],
+			source=request.POST['stripeToken']
+
+
+		)
+		charge = stripe.Charge.create(
+			customer=customer,
+			amount=amount*100,
+			currency='usd',
+			description="donation"
+
+		)
 
 	return redirect(reverse('success', args=[amount]))
 
